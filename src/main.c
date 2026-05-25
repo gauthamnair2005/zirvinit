28fd4164f33e20870e1a082335bb6bdba9445427
Gautham Nair <gautham.nair.2005@gmail.com>
2026-05-25 09:27:39 +0530

    ZirvUI desktop as default, V-key shell toggle, boot banner refresh

--- src/main.c
+++ src/main.c
@@ -2,27 +2,54 @@
 #include <unistd.h>
 #include <string.h>
 
+#define KEY_V 0x19
+
 static void mosix_banner(void)
 {
-    printf("\n  Z I R V I N I T\n");
-    printf("  ---------------\n");
-    printf("  MOSIX Compliant PID 1\n");
-    printf("  Lead Developer: Gautham Nair\n\n");
+    printf("\n");
+    printf("  +--------------------------------------+\n");
+    printf("  |                                      |\n");
+    printf("  |           Z I R V I U M              |\n");
+    printf("  |    MOSIX Operating System            |\n");
+    printf("  |                                      |\n");
+    printf("  +--------------------------------------+\n");
+    printf("\n");
 }
 
 int main(void)
 {
     mosix_banner();
-    printf("[ INIT ] Starting MOSIX lifecycle...\n");
-    printf("[ INIT ] Verifying /zirv namespace...\n");
-    printf("[ INIT ] Launching ZirvShell...\n");
+    printf("[INIT] Starting MOSIX lifecycle...\n");
+    printf("[INIT] Verifying /zirv namespace...\n");
+    printf("[INIT] Press 'v' for CLI shell...\n");
 
-    char *argv[] = { "/bin/shell", NULL };
-    char *envp[] = { NULL };
+    int launch_shell = 0;
+    for (int i = 0; i < 50; i++) {
+        key_event_t ev;
+        if (read_keys(&ev) == 0 && ev.pressed && ev.keycode == KEY_V) {
+            launch_shell = 1;
+            break;
+        }
+        msleep(10);
+    }
 
-    execve("/bin/shell", argv, envp);
+    if (launch_shell) {
+        printf("[INIT] Launching ZirvShell...\n\n");
+        char *argv[] = { "/bin/shell", NULL };
+        char *envp[] = { NULL };
+        execve("/bin/shell", argv, envp);
+    } else {
+        printf("[INIT] Launching ZirvUI desktop...\n\n");
+        char *argv[] = { "/bin/zirvui", NULL };
+        char *envp[] = { NULL };
+        execve("/bin/zirvui", argv, envp);
 
-    printf("[ INIT ] execve failed!\n");
+        printf("[INIT] GUI exited, launching shell...\n\n");
+        char *argv2[] = { "/bin/shell", NULL };
+        execve("/bin/shell", argv2, envp);
+    }
+
+    printf("[INIT] execve failed!\n");
     for (;;) ;
     return 0;
 }
